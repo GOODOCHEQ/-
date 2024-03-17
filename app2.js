c945c01a7ebaf5be7e7aa4b7ac6eefa1d1b1dbac
GOODOCHEQ <147840010+GOODOCHEQ@users.noreply.github.com>
2024-03-17 23:14:44 +0300

    Update app2.js

--- app2.js
+++ app2.js
@@ -5,8 +5,6 @@ tg.expand();
 tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
 
-let item = "";
-
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
 let btn3 = document.getElementById("btn3");
@@ -14,82 +12,30 @@ let btn4 = document.getElementById("btn4");
 let btn5 = document.getElementById("btn5");
 let btn6 = document.getElementById("btn6");
 
+function sendData(item) {
+    tg.sendData(item);
+}
+
 btn1.addEventListener("click", function(){
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-    }
-    else {
-        tg.MainButton.setText("Добавлено");
-        item = "1";
-        tg.MainButton.show();
-    }
+    sendData("1");
 });
 
 btn2.addEventListener("click", function(){
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-    }
-    else {
-        tg.MainButton.setText("Добавлено");
-        item = "2";
-        tg.MainButton.show();
-    }
+    sendData("2");
 });
 
 btn3.addEventListener("click", function(){
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-    }
-    else {
-        tg.MainButton.setText("Добавлено");
-        item = "3";
-        tg.MainButton.show();
-    }
+    sendData("3");
 });
 
 btn4.addEventListener("click", function(){
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-    }
-    else {
-        tg.MainButton.setText("Добавлено");
-        item = "4";
-        tg.MainButton.show();
-    }
+    sendData("4");
 });
 
 btn5.addEventListener("click", function(){
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-    }
-    else {
-        tg.MainButton.setText("Добавлено");
-        item = "5";
-        tg.MainButton.show();
-    }
+    sendData("5");
 });
 
 btn6.addEventListener("click", function(){
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-    }
-    else {
-        tg.MainButton.setText("Добавлено");
-        item = "6";
-        tg.MainButton.show();
-    }
+    sendData("6");
 });
-
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
-    tg.sendData(item);
-});
-
-
-let usercard = document.getElementById("usercard");
-
-let p = document.createElement("p");
-
-p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
-
-usercard.appendChild(p);
